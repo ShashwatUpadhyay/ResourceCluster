8d4d288fd9a1266b131ae0e9c62066e1372bf6f5
ShashwatUpadhyay <shashwatup619@gmail.com>
2025-08-27 00:45:50 +0530

    Api intigrated at home page

--- resource/views.py
+++ resource/views.py
@@ -1,14 +1,26 @@
+from django.db.models import F
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import ResourceSerializer
 from .models import Resource
+from django.db.models import Q
 
 # Create your views here.
 
 class ListResourcesAPIView(APIView):
     def get(self, request):
+        course = request.GET.get('course')
+        subject = request.GET.get('subject')
+        session = request.GET.get('session')
+        
         resources = Resource.objects.all()
+        if course:
+            resources = resources.filter(course__uid=course)
+        if subject:
+            resources = resources.filter(subject__uid=subject)
+        if session:
+            resources = resources.filter(session__uid=session)
         serializer = ResourceSerializer(resources, many=True)
         return Response({
             'status': 200,
